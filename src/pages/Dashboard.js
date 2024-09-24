a60601b745b54bd9f415e1be2341093190ac67dc
cee-diksha <cee.diksha@gmail.com>
2024-09-24 17:24:34 +0530

    dashboard ui modified

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -60,6 +60,18 @@ const Dashboard = () => {
   return (
     <>
         <div  className='dashboard-wrapper'>
+        <div style={{ display: "flex", alignItems: "center", justifyContent: "space-between", height: "12%", width: "96%"}}>
+            <div style={{display: "flex", alignItems: "center", justifyContent: "space-between", width: "auto"}}>
+                <img style={{height: "100px"}} src={shipcrest} alt="ship" />
+                <h2 id="dashboard-heading">Ship Name</h2>
+            </div>
+            <DropDown cardData={cardData} />
+            <div className='imgWrapper'>
+                {isLogin ? <img src={user} alt="user-img" className='img'/> : <Link to="/login" className='link'><h6 className='login'>Login</h6></Link>}
+                <Link to={isLogin ? "/settings" : "#"} 
+                style={{ pointerEvents: isLogin ? 'auto' : 'none', opacity: isLogin ? 1 : 0.5 }}><img src={settings} alt="settings" className='img'/></Link>
+            </div>
+            </div>
             <div className='dashboard-main-screen'>
             <div className='dashboard-middle'>
                 <div className='dashboard-content-summary'>
@@ -73,7 +85,7 @@ const Dashboard = () => {
                 <div style={{backgroundColor: "#ffffff", width: "100%", borderRadius: "10px", display: "flex", alignItems: "center", justifyContent: 'center'}}>
                     <TempChart />
                 </div>
-                <div style={{backgroundColor: "#ffffff", width: "100%", borderRadius: "10px", display: "flex", alignItems: "center", justifyContent: 'center'}}>
+                <div style={{backgroundColor: "#ffffff", width: "100%", borderRadius: "10px", display: "flex", alignItems: "center", justifyContent: 'center', marginTop: "14px"}}>
                     <BatteryChart />
                 </div>
                 <div className='dashboard-content-comm'>
@@ -81,81 +93,35 @@ const Dashboard = () => {
                 </div>
             </div>
             {/* -------------------------------------------------------- */}
-            <div className='dashboard-header-cards'>
-                <div style={{ display: "flex", alignItems: "center", justifyContent: "space-between", height: "12%"}}>
-                    <div style={{display: "flex", alignItems: "center", justifyContent: "space-between", width: "auto"}}>
-                        <img style={{height: "100px"}} src={shipcrest} alt="ship" />
-                        <h2 id="dashboard-heading">Ship Name</h2>
-                    </div>
-                    
-                    <div className='imgWrapper'>
-                        {isLogin ? <img src={user} alt="user-img" className='img'/> : <Link to="/login" className='link'><h6 className='login'>Login</h6></Link>}
-                        <Link to={isLogin ? "/settings" : "#"} 
-                        style={{ pointerEvents: isLogin ? 'auto' : 'none', opacity: isLogin ? 1 : 0.5 }}><img src={settings} alt="settings" className='img'/></Link>
-                    </div>
-                </div>
-                <div className='dashboard-main-wrapper'>
-                    {/* -------------------------------------------------------- */}
-                    <div className='main-dashboard-wrapper'>
-                    <DropDown cardData={cardData} />
-                    <div className='fixed-content'>
-                        {console.log(cardData, "checking if updates")}
-                            {cardData.map((item) => {
-                                return (
-                                    item.status === "danger" && <Link className='link-style' to={`info/${item.node_name}`}><Card key={item.id} item={item} /></Link>
-                                ) 
-                            })}
-                            {cardData.map((item) => {
-                                return (
-                                    item.status === "orange" && <Link className='link-style' to={`info/${item.node_name}`}><Card key={item.id} item={item} /></Link>
-                                ) 
-                            })}
-                            {cardData.map((item) => {
-                                return (
-                                    item.status === "yellow" && <Link className='link-style' to={`info/${item.node_name}`}><Card key={item.id} item={item} /></Link>
-                                ) 
-                            })}
-                        </div>
-                        {deckInfo.length > 0 && <div className='deck-display-grid'>
-                            {console.log(deckInfo, "checkingdeckinfo")}
-                       {deckInfo.map((item, index) => {
-                        return (
-                            <DeckDashboardPageDiv key={index} data={item} />
-                        )
-                       })} 
-                    </div>}
-                    {/* <div className='dashboard-cardata'>
-                        <div className='scrollable-content'>
-                            {cardData.map((item) => {
-                                return (
-                                item.status === "success" && <Link className='link-style' to={`info/${item.node_name}`}><Card key={item.id} item={item} /></Link>
-                                ) 
-                            })}
-                            {cardData.map((item) => {
-                                return (
-                                    item.status === "success" &&  <Link className='link-style' to={`info/${item.node_name}`}><Card key={item.id + '-duplicate'} item={item} /></Link>
-                                ) 
-                            })}
-                        </div>
-                    </div> */}
-                    </div>
-
-                    {/* -------------------------------------------------------- */}
-
-                    {/* <div className='dashboard-alerts'>
+                <div className='main-dashboard-wrapper'>
+                <div className='fixed-content'>
+                    {console.log(cardData, "checking if updates")}
                     {cardData.map((item) => {
                         return (
-                            <Link className='link-style' to={`info/${item.node_name}`}>
-                                <div className="hover-container">
-                                    <AlertCircle item={item} />
-                                    <div className="hover-text">{item.node_name}</div>
-                                </div>
-                            </Link>
-                        )
+                            item.status === "danger" && <Link className='link-style' to={`info/${item.node_name}`}><Card key={item.id} item={item} /></Link>
+                        ) 
                     })}
-                    </div> */}
+                    {cardData.map((item) => {
+                        return (
+                            item.status === "orange" && <Link className='link-style' to={`info/${item.node_name}`}><Card key={item.id} item={item} /></Link>
+                        ) 
+                    })}
+                    {cardData.map((item) => {
+                        return (
+                            item.status === "yellow" && <Link className='link-style' to={`info/${item.node_name}`}><Card key={item.id} item={item} /></Link>
+                        ) 
+                    })}
                 </div>
-            </div>
+                    {deckInfo.length > 0 && <div className='deck-display-grid'>
+                        {console.log(deckInfo, "checkingdeckinfo")}
+                    {deckInfo.map((item, index) => {
+                    return (
+                        <DeckDashboardPageDiv key={index} data={item} />
+                    )
+                    })} 
+                </div>}
+
+                </div>
             </div>
             {/* -------------------------------------------------------- */}
             <div className="dashboard-sticky">
@@ -167,3 +133,36 @@ const Dashboard = () => {
 }
 
 export default Dashboard
+
+
+
+    // {/* <div className='dashboard-alerts'>
+    // {cardData.map((item) => {
+    //     return (
+    //         <Link className='link-style' to={`info/${item.node_name}`}>
+    //             <div className="hover-container">
+    //                 <AlertCircle item={item} />
+    //                 <div className="hover-text">{item.node_name}</div>
+    //             </div>
+    //         </Link>
+    //     )
+    // })}
+    // </div> */}
+
+
+
+
+    //     {/* <div className='dashboard-cardata'>
+    //     <div className='scrollable-content'>
+    //         {cardData.map((item) => {
+    //             return (
+    //             item.status === "success" && <Link className='link-style' to={`info/${item.node_name}`}><Card key={item.id} item={item} /></Link>
+    //             ) 
+    //         })}
+    //         {cardData.map((item) => {
+    //             return (
+    //                 item.status === "success" &&  <Link className='link-style' to={`info/${item.node_name}`}><Card key={item.id + '-duplicate'} item={item} /></Link>
+    //             ) 
+    //         })}
+    //     </div>
+    // </div> */}
